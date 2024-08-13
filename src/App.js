ecccb1555b9d29b3293044091be75d1cbb23de52
baejimil <qowhdgus629@naver.com>
2024-08-13 10:50:48 +0900

    RockScissorsPaper first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Box from './component/Box';
 
+const choice = {
+  rock : {
+    name : "Rock",
+    img : "https://m.media-amazon.com/images/I/51rOMX5z40L.jpg"
+  },
+  scissors : {
+    name : "Scissors",
+    img : "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT7zluUaMxNynokVgHhNnZ8kkfOXP60e5ZBiB9XtY8Nm18Cjs9_ZsLJd-ATis74Tjll23LEb9WVopmeXgTeveM"
+  },
+  paper : {
+    name : "Paper",
+    img : "https://m.media-amazon.com/images/I/61OorFhm6SL._AC_UF894,1000_QL80_.jpg"
+  }
+}
 function App() {
+
+  const [userSelect, setUserSelect] = useState(null)
+
+  const play=(userChoice)=> {
+    setUserSelect(choice[userChoice])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='main'>
+        <Box title='You' item={userSelect} />
+        {/* <Box title='Computer' /> */}
+      </div>
+
+      <div className='main'>
+        <button onClick={ () => play("scissors")}>가위</button>
+        <button onClick={ () => play("rock")}>바위</button>
+        <button onClick={ () => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
